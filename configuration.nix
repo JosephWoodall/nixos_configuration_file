3a50b58452306aa60f31c59689d52c1f270a773f
Joseph Woodall <56406362+JosephWoodall@users.noreply.github.com>
2025-12-09 10:25:19 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,9 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
+  ################################################################################
+  # NEOVIM CONFIGURATION
+  ################################################################################
   nvimConfig = pkgs.writeText "init.lua" ''
     local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
     if not vim.loop.fs_stat(lazypath) then
@@ -31,58 +34,58 @@ let
     -- Load plugins via lazy.nvim
     require("lazy").setup({
     
-    	-- LSP
-{
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "rust_analyzer", "pyright" },
-      automatic_installation = true,
-    })
+      -- LSP
+      {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+          "williamboman/mason.nvim",
+          "williamboman/mason-lspconfig.nvim",
+        },
+        config = function()
+          require("mason").setup()
+          require("mason-lspconfig").setup({
+            ensure_installed = { "rust_analyzer", "pyright" },
+            automatic_installation = true,
+          })
 
-    -- Rust Analyzer
-vim.lsp.config("rust_analyzer", {
-  settings = {
-    ["rust-analyzer"] = {
-      cargo = { allFeatures = true },
-      checkOnSave = { command = "clippy" },
-    },
-  },
-})
+          -- Rust Analyzer
+          vim.lsp.config("rust_analyzer", {
+            settings = {
+              ["rust-analyzer"] = {
+                cargo = { allFeatures = true },
+                checkOnSave = { command = "clippy" },
+              },
+            },
+          })
 
--- Pyright
-vim.lsp.config("pyright", {})
+          -- Pyright
+          vim.lsp.config("pyright", {})
 
-    -- LSP keymaps
-    local opts = { noremap=true, silent=true }
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-  end,
-},
-
--- Treesitter
-{
-  "nvim-treesitter/nvim-treesitter",
-  run = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {"python", "rust", "json" }, 
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
+          -- LSP keymaps
+          local opts = { noremap=true, silent=true }
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        end,
       },
-    })
-  end,
-},
-    	
-    	-- Colorscheme
+
+      -- Treesitter
+      {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+        config = function()
+          require("nvim-treesitter.configs").setup({
+            ensure_installed = {"python", "rust", "json" }, 
+            highlight = {
+              enable = true,
+              additional_vim_regex_highlighting = false,
+            },
+          })
+        end,
+      },
+        
+      -- Colorscheme
       {
         "folke/tokyonight.nvim",
         lazy = false,
@@ -138,36 +141,35 @@ vim.lsp.config("pyright", {})
         end,
       },
       
-      --Auto complete brackets/parenthesis
-        {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-      require("nvim-autopairs").setup {}
-    end,
-  },
-  
-	-- Formatter
-	{
-    "stevearc/conform.nvim",
-    config = function()
-      require("conform").setup({
-        formatters_by_ft = {
-          lua = { "stylua" },
-          python = { "black" },
-          rust = { "rustfmt" },
-          -- add more per‑language formatters
-        },
-        format_on_save = {
-          timeout_ms = 500,
-          lsp_format = "fallback",
-        },
-      })
-    end,
-  },
-  
-  	--Which Key
-{
+      -- Auto complete brackets/parenthesis
+      {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+          require("nvim-autopairs").setup {}
+        end,
+      },
+      
+      -- Formatter
+      {
+        "stevearc/conform.nvim",
+        config = function()
+          require("conform").setup({
+            formatters_by_ft = {
+              lua = { "stylua" },
+              python = { "black" },
+              rust = { "rustfmt" },
+            },
+            format_on_save = {
+              timeout_ms = 500,
+              lsp_format = "fallback",
+            },
+          })
+        end,
+      },
+      
+      -- Which Key
+      {
         "folke/which-key.nvim",
         event = "VeryLazy",
         opts = {
@@ -189,57 +191,99 @@ vim.lsp.config("pyright", {})
             { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Buffers" },
             { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
             { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live Grep" },
-
           })
         end,
       },
-    	
-    	})
+    })
   '';
- in
+  
+  ################################################################################
+  # HARDWARE DETECTION
+  ################################################################################
+  hasNvidiaGpu = builtins.pathExists /sys/module/nvidia;
+
+in
 {
+  ################################################################################
+  # IMPORTS
+  ################################################################################
   imports = [ ./hardware-configuration.nix ];
 
+  ################################################################################
+  # BOOT CONFIGURATION
+  ################################################################################
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  ################################################################################
+  # NETWORKING
+  ################################################################################
   networking.hostName = "redleadr";
   networking.networkmanager.enable = true;
 
+  ################################################################################
+  # GRAPHICS & GPU
+  ################################################################################
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
+  
+  # Conditionally enable NVIDIA drivers if GPU is detected
+  services.xserver.videoDrivers = lib.mkIf hasNvidiaGpu [ "nvidia" ];
+  
+  hardware.nvidia = lib.mkIf hasNvidiaGpu {
     open = true;
     modesetting.enable = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
+  ################################################################################
+  # LOCALIZATION
+  ################################################################################
   time.timeZone = "America/New_York";
   i18n.defaultLocale = "en_US.UTF-8";
+
+  ################################################################################
+  # KEYBOARD
+  ################################################################################
   services.xserver.xkb.layout = "us";
 
+  ################################################################################
+  # USER CONFIGURATION
+  ################################################################################
   users.users.redleadr = {
     isNormalUser = true;
     description = "redleadr";
     extraGroups = [ "networkmanager" "wheel" "video" ];
   };
 
+  ################################################################################
+  # NIXPKGS CONFIGURATION
+  ################################################################################
   nixpkgs.config.allowUnfree = true;
 
+  ################################################################################
+  # DISPLAY SERVER & DESKTOP ENVIRONMENT
+  ################################################################################
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
+  
+  # Auto-login
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "redleadr";
+  
+  # Remove default GNOME terminals
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-console
+    gnome-terminal
+  ];
 
-
-environment.systemPackages = with pkgs; [
-
-#############
-# DEVELOPMENT
-############
+  ################################################################################
+  # SYSTEM PACKAGES
+  ################################################################################
+  environment.systemPackages = with pkgs; [
+    # Development Tools
     (neovim.override {
       configure = {
         customRC = ''
@@ -247,69 +291,71 @@ environment.systemPackages = with pkgs; [
         '';
       };
     })
-# LSP servers
-  pyright
-  rust-analyzer
- # Formatters
-  nodePackages.prettier
-  black
-  rustfmt
-  nixfmt-classic
- # Clipboard
- xclip
- # Compiler
- gcc
+    pyright              # Python LSP
+    rust-analyzer        # Rust LSP
+    nodePackages.prettier # Multi-language formatter
+    black                # Python formatter
+    rustfmt              # Rust formatter
+    nixfmt-classic       # Nix formatter
+    xclip                # Clipboard utility
+    gcc                  # C compiler (required for Treesitter)
 
-##############
-# IN-MEMORY DB
-##############
-redis 
+    # In-Memory Database
+    redis
 
-##########
-# TERMINAL
-##########
-ghostty
+    # Terminal Emulator
+    ghostty
 
-###########
-# UTILITIES
-###########
-lazygit
-fzf
-ripgrep
-bottom
-fastfetch
+    # Utilities
+    lazygit              # Git TUI
+    fzf                  # Fuzzy finder
+    ripgrep              # Fast grep alternative
+    bottom               # System monitor
+    fastfetch            # System info
+    obsidian             # Note-taking
 
-##################
-# INTERNET BROWSER
-##################
-google-chrome
+    # Web Browser
+    google-chrome
 
-#######
-# STEAM
-#######
-steam 
-
-];
+    # Gaming
+    steam
+  ];
 
   ################################################################################
-  # Environment variables
+  # ENVIRONMENT VARIABLES
   ################################################################################
   environment.variables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
   };
 
+  ################################################################################
+  # PROGRAMS & SERVICES
+  ################################################################################
+  
+  # Git
   programs.git.enable = true;
+  
+  # Bluetooth
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
   services.blueman.enable = true;
+  
+  # Steam
   programs.steam.enable = true;
+  
+  # Dconf (GNOME settings)
+  programs.dconf.enable = true;
+  programs.dconf.profiles.user.databases = [{
+    settings = {
+      "org/gnome/desktop/applications/terminal" = {
+        exec = "ghostty";
+      };
+    };
+  }];
 
+  ################################################################################
+  # SYSTEM VERSION
+  ################################################################################
   system.stateVersion = "25.05";
 }
-
-
-
-
-
-
